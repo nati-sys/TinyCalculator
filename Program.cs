ef2ac0d6ce50f412d86a5cb15da763564090ea35
Aigner Natalia <N.Aigner@htlvb.at>
2026-03-20 09:12:04 +0100

    Colored Calculations

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("2. Operand: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"{a} + {b} = {a + b}");
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine($"{a} - {b} = {a - b}");
+Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine($"{a} * {b} = {a * b}");
+Console.ForegroundColor = ConsoleColor.Blue;
 if (b == 0)
 {
     Console.WriteLine($"{a} / {b} = undefiniert/unendlich");
@@ -17,3 +21,4 @@ else
 {
     Console.WriteLine($"{a} / {b} = {a / b}");
 }
+Console.ResetColor();
