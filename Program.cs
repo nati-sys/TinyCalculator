ffa6fb860429c51ccb5f9a2d3724985412bd8c90
Aigner Natalia <N.Aigner@htlvb.at>
2026-03-20 10:29:02 +0100

    Simplify double division

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿Console.WriteLine("== Tiny calculator ==");
+﻿using System.Text;
 
+Console.WriteLine("== Tiny calculator ==");
+
+Console.OutputEncoding = Encoding.UTF8;
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.Write("1. Operand: ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -13,12 +16,5 @@ Console.WriteLine($"{a} - {b} = {a - b}");
 Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine($"{a} * {b} = {a * b}");
 Console.ForegroundColor = ConsoleColor.Blue;
-if (b == 0)
-{
-    Console.WriteLine($"{a} / {b} = undefiniert/unendlich");
-}
-else
-{
-    Console.WriteLine($"{a} / {b} = {(double)a / b}");
-}
+Console.WriteLine($"{a} / {b} = {(double)a / b}");
 Console.ResetColor();
