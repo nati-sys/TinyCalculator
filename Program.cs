a2865d977e6926fcc8c48e7f9dfecb6284fe280b
Aigner Natalia <N.Aigner@htlvb.at>
2026-03-20 09:09:39 +0100

    Colored operands

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.Write("1. Operand: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("2. Operand: ");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
 Console.WriteLine($"{a} + {b} = {a + b}");
 Console.WriteLine($"{a} - {b} = {a - b}");
 Console.WriteLine($"{a} * {b} = {a * b}");
